a46a59cfb69d5d43ce4cefd354ba7064019ef716
BaronVonBuron <andbur01@easv365.dk>
2024-11-18 12:52:46 +0100

    Initial Setup incl. Test project

---
+++ FixPoint-Backend-Test/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace FixPoint_Backend_Test;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
